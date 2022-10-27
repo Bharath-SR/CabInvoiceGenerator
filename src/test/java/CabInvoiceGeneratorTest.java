7a0867364c23c9f71732d40c8a53f5f9223dc965
Bharath-SR <bharath.s0941@gmail.com>
2022-10-27 06:19:03 +0530

    UC1-Calculate Fare is added

--- src/test/java/CabInvoiceGeneratorTest.java
+++ src/test/java/CabInvoiceGeneratorTest.java
@@ -1,2 +1,24 @@
+import com.bridgeLabz.CabInvoiceGenerator.CabInvoiceGenerator;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CabInvoiceGeneratorTest {
+
+    @Test
+    public void givenDistanceAndTime_shouldReturnTotalFare(){
+        CabInvoiceGenerator invoiceGen = new CabInvoiceGenerator();
+        double distance=2.0;
+        int time=5;
+        double fare = invoiceGen.calculateFare(distance, time );
+        Assert.assertEquals(25,fare,0.0);
+    }
+    @Test
+    public void givenDistanceAndTime_shouldReturnMinFare(){
+        CabInvoiceGenerator invoiceGen = new CabInvoiceGenerator();
+        double distance=0.1;
+        int time=1;
+        double fare = invoiceGen.calculateFare(distance, time );
+        Assert.assertEquals(5,fare,0.0);
+    }
+
 }
